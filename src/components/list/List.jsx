ee3cb3ea3f6b3ee510081fb89ff60ccd406aff6a
kimminseok <kms3288381@naver.com>
2022-10-05 17:25:40 +0900

    삭제 구현

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -7,7 +7,7 @@ function List({ todoList }) {
   return (
     <div>
       {todoList.map((list) => (
-        //map 함수를 사용해 todoList만큼 반복,
+        //map 함수를 사용해 todoList만큼 반복
         <Todo list={list} />
         //Todo라는 파일에 list라는 이름으로 list 값을 보내준다.
       ))}
